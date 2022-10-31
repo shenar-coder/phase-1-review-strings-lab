ce7ba87ecaa2f59ccdfcc87f6cdda6eab8b70fc1
shinar3@gmail.com <110408797+shenar-coder@users.noreply.github.com>
2022-10-31 13:41:24 -0400

    commit tree 72e6d8e74459a81d1dc152b61082fef293fc08c2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your code in this file!
+/*const currentUser = 'Grace Hopper';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, $(currenuser}`;
+*/
+
+const currentUser = 'Grace Hopper';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
